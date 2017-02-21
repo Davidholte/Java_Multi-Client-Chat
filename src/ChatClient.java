1028cdad8df9f80b1d75e31b8cd868fd6491033a
Dave <davidholte123@gmail.com>
2017-02-21 16:59:24 +0100

    Der er hul igennem

---
+++ src/ChatClient.java
@@ -0,0 +1,74 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ * Created by Dave on 21/02/2017.
+ */
+public class ChatClient {
+
+    private static InetAddress host;
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) {
+
+        try {
+            host = InetAddress.getLocalHost();
+        }
+        catch (UnknownHostException uex) {
+            System.out.println("NO HOST");
+            System.exit(1);
+        }
+        accessServer();
+    }
+
+    private static void accessServer() {
+
+        Socket socket = null;
+
+        try {
+            socket = new Socket(host, PORT);
+
+            Scanner read = new Scanner(socket.getInputStream());
+            PrintWriter write = new PrintWriter(socket.getOutputStream(), true);
+
+            // set up stream for key entry
+            Scanner userInput = new Scanner(System.in);
+
+            String message = "";
+            String response = "";
+
+
+
+            while (!message.equals("*CLOSE*")) {
+                System.out.print("Enter Message: ");
+                message = userInput.nextLine();
+                write.println(message);
+
+                response = read.nextLine();
+                System.out.println("\nSERVER> "+ response);
+
+
+
+            }
+
+        }
+        catch (IOException ioex) {
+            ioex.printStackTrace();
+        }
+        finally {
+
+            try {
+                System.out.println("\n* Closing connection... *");
+                socket.close();
+            }
+            catch (IOException ioex) {
+                System.out.println("Unable to disconnect!");
+                System.exit(1);
+            }
+        }
+    }
+}
---
+++ src/ChatServer.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashSet;
+
+/**
+ * Created by Dave on 21/02/2017.
+ */
+public class ChatServer {
+
+
+    /** Sets and Variables */
+    // Set of names of all the clients
+    private static final int PORT = 8080;
+
+    private static HashSet<String> clientNames = new HashSet<>();
+
+    private static HashSet<PrintWriter> printWriters = new HashSet<>();
+
+
+    /** Main method to create ClientHandler threads */
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Server is operational");
+        ServerSocket listener = new ServerSocket(PORT);
+        try {
+            while (true) {
+                new ClientHandler(listener.accept()).start();
+            }
+        }
+        finally {
+            listener.close();
+        }
+    }
+
+
+    /** ClientHandler class extended from Thread Class to handle each customer paralleled */
+    private static class ClientHandler extends Thread {
+
+        private String name;
+        private PrintWriter write;
+        private BufferedReader read;
+        private Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+
+    /** How each ClientHandler thread processes each client */
+        public void run() {
+
+            try {
+                read = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                write = new PrintWriter(socket.getOutputStream(), true);
+
+                while (true) {
+                    write.println("SUBMITNAME");
+                    name = read.readLine();
+
+                    if (name == null) {
+                        return;
+                    }
+                    else {
+                        synchronized (clientNames) {
+                            if (!clientNames.contains(name)) {
+                                clientNames.add(name);
+                                break;
+                            }
+                        }
+                    }
+                }
+                write.println("NAMEACCEPTED");
+                printWriters.add(write);
+
+                while (true) {
+                    String input = read.readLine();
+
+                    if (input == null) {
+                        return;
+                    }
+                    for (PrintWriter writer : printWriters) {
+                        writer.println("MESSAGE " + name + ": " + input);
+                    }
+                }
+            }
+            catch (IOException ioException) {
+                System.out.println(ioException);
+            }
+            finally {
+                if (name != null) {
+                    clientNames.remove(name);
+                }
+                if (write != null) {
+                    printWriters.remove(write);
+                }
+
+                try {
+                    socket.close();
+                }
+                catch (IOException ioException) {
+                 //   System.out.println(ioException);
+                }
+            }
+        }
+
+    }
+
+
+}
